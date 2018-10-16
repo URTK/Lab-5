b695cc45b9e9ee01f4c033bdc2fdd9a56764abb7
УРТК <43788393+URTK@users.noreply.github.com>
2018-10-16 06:22:56 +0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,22 +3,30 @@ import math
 masX = []
 masY = []
 
-# inputmas = 0
 n = 1
+
+    # Массив X
 
 for i in range(19):
     print('Введите элемент массива', n)
     masX.append(int(input()))
     n += 1
-    
+print(' MasX ', masX)
+
+    # Массив Y
+
 for i in range(19):
     y = 0,5 * math.log(masX[i])
     masY.append(y)
+print(' MasY ', masY)  
+
+    # Поиск наибольшего числа, кратного 3 и вывод его индекса
     
 MaxY = masY[0]
 
 for i in range(19):
     if masY[i]>MaxY:
         MaxY = masY[i]
+        if MaxY / 3 % 10
         
-print(MaxY)
+print(' Наивысший элемент ', MaxY)
